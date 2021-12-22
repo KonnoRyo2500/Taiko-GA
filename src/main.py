ccfdf87ee136519b7c49f96ec2df027c6867fed0
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2021-12-22 21:54:04 +0900

    TJAファイルのパスをコマンドラインで指定できるようにした

--- src/main.py
+++ src/main.py
@@ -1,16 +1,31 @@
 # 太鼓さん次郎GAアプリ メイン処理
 
+import argparse
+
 from training_data import make_training_data
 from jiro import play_chart
 from ga import GA
 from const_val import *
 
+# コマンドライン引数の解析
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--tja_path',
+        '-t',
+        type=str,
+        required=True,
+        help='譜面ファイル(.tja)のパス。')
+    args = parser.parse_args()
+
+    return args
+
 # メイン処理
 def main():
-    # TODO: コマンドライン引数にする
-    tja_path = 'C:\\Users\\enc91\\Desktop\\hobby\\taiko\\sample_tja\\release.tja'
-    training_data = make_training_data(tja_path)
-    g = GA(training_data=training_data)
+    args = parse_args()
+
+    training_data = make_training_data(args.tja_path)
+    g = GA(training_data=training_data, n_genes=1)
     gene = g.genes[0]
     score = g.scores[0]
     chart = []
@@ -21,7 +36,7 @@ def main():
         timing += SEC_PER_SAMPLING
 
     print(f'Expected score: {score}')
-    play_chart(chart, tja_path)
+    play_chart(chart, args.tja_path)
 
 if __name__ == '__main__':
     main()
