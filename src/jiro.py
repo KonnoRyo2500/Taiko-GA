a21cca8643030b4e4c9fcb90146c646a7cd05e0c
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2021-12-27 22:53:03 +0900

    演奏処理を高速化 次の音符を叩くまでの待ち時間が0である場合、time.sleep関数を呼び出さないようにした

--- src/jiro.py
+++ src/jiro.py
@@ -26,7 +26,8 @@ def play_chart(chart, tja_path):
     for note_info in chart:
         note_type, timing = note_info
         wait_time = timing - now + start_time
-        time.sleep(wait_time if wait_time >= 0 else 0)
+        if wait_time > 0:
+            time.sleep(wait_time)
         _play_note(note_type)
         now = time.time()
 
